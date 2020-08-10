628f97adcea832da124d65fbf23a2d423391de81
ayokinya <jkang@c2r4s5.42seoul.kr>
2020-08-10 21:32:12 +0900

    ADD : CPP04 example added

--- CPP_MODULE_04/ex00/main.cpp
+++ CPP_MODULE_04/ex00/main.cpp
@@ -10,15 +10,21 @@ int main()
 	std::cout << robert << jim << joe;
 	robert.polymorph(jim);
 	robert.polymorph(joe);
-	// std::cout << "==================" << std::endl;
-	// std::cout << "Additional Tests start!" << std::endl;
-	// std::cout << "==================" << std::endl;
-	// Sorcerer tmp(robert);
-	// Victim x(jim);
-	// Peon y(joe);
-	// std::cout << x << y;
-	// robert.polymorph(x);
-	// robert.polymorph(y);
+	std::cout << "==================" << std::endl;
+	std::cout << "Additional Tests start!" << std::endl;
+	std::cout << "==================" << std::endl;
+	Sorcerer tmp(robert);
+	Victim x(jim);
+	Peon y(joe);
+	std::cout << tmp << x << y;
+	x = jim;
+	tmp.polymorph(x);
+	x.getPolymorphed();
+	tmp = robert;
+	y = joe;
+	tmp.polymorph(y);
+	y.getPolymorphed();
+	std::cout << "==================" << std::endl;
 	return 0;
 }
 
--- CPP_MODULE_04/ex01/main.cpp
+++ CPP_MODULE_04/ex01/main.cpp
@@ -11,19 +11,66 @@ int main()
 	Character* me = new Character("me");
 	std::cout << *me;
 	Enemy* b = new RadScorpion();
+	std::cout << "enemy b's HP is " << b->getHP() << std::endl;
 	AWeapon* pr = new PlasmaRifle();
 	AWeapon* pf = new PowerFist();
 	me->equip(pr);
 	std::cout << *me;
 	me->equip(pf);
 	me->attack(b);
+	std::cout << "enemy b's HP is " << b->getHP() << std::endl;
 	std::cout << *me;
 	me->equip(pr);
 	std::cout << *me;
 	me->attack(b);
+	std::cout << "enemy b's HP is " << b->getHP() << std::endl;
 	std::cout << *me;
 	me->attack(b);
+	std::cout << "enemy b's HP is " << b->getHP() << std::endl;
 	std::cout << *me;
+	
+	delete me;
+	std::cout << "==================" << std::endl;
+	std::cout << "Additional Tests start!" << std::endl;
+	std::cout << "==================" << std::endl;
+
+	Character you("you");
+
+	Enemy *s = new SuperMutant();
+
+	you.equip(pr);
+	std::cout << you;
+	you.equip(pf);
+	std::cout << you;
+	you.equip(NULL);
+	std::cout << you;
+
+	you.attack(s);
+	std::cout << "supermutant s's HP is " << s->getHP() << std::endl;
+	you.equip(pr);
+	you.attack(s);
+	std::cout << "supermutant s's HP is " << s->getHP() << std::endl;
+	you.equip(pf);
+	you.attack(s);
+	std::cout << "supermutant s's HP is " << s->getHP() << std::endl;
+
+	Enemy* e = new Enemy(73, "devil");
+
+	you.attack(e);
+	std::cout << "supermutant s's HP is " << e->getHP() << std::endl;
+
+	RadScorpion rs1;
+	RadScorpion rs2(rs1);
+	std::cout << rs1.getType() << " " << rs2.getType() << std::endl;
+	rs2 = rs1;
+	std::cout << rs1.getType() << " " << rs2.getType() << std::endl;
+	
+	SuperMutant sm1;
+	SuperMutant sm2(sm1);
+	std::cout << sm1.getType() << " " << sm2.getType() << std::endl;
+	sm2 = sm1;
+	std::cout << sm1.getType() << " " << sm2.getType() << std::endl;
+
 	return 0;
 
 }
