7b47995501a4baa9202f4c60b9e477de29fdaae5
ayokinya <ayokinya@gmail.com>
2020-08-14 18:39:38 +0900

    CPP07 ex02 leaks ongoing

--- CPP_MODULE_07/ex02/Array.hpp
+++ CPP_MODULE_07/ex02/Array.hpp
@@ -9,27 +9,82 @@ template <typename T>
 class Array
 {
 	private:
-			T *arr;
+			unsigned int	arr_len;
+			T				*arr;
 	public:
 			Array();
 			~Array();
 			Array(unsigned int n);
-			Array(Array const &copy); // deep copy
-			Array& operator=(Array const &array); // deep copy
+			Array(Array const &copy);
+			Array& operator=(Array const &array);
 			std::exception IndexOutOfRange() const;
-			int & operator[](const unsigned int index);
-			void * operator new[](size_t sz)
-			{
-				try
-				{
-					return (new T[sz]);
-				}
-				catch(std::exception const &e)
-				{
-
-				}
-			}
-			void	size(void);
+			T & operator[](unsigned int index) const;
+			unsigned int	size(void) const;
 };
+
+template <typename T>
+Array<T>::Array() : arr_len(0), arr(nullptr)
+{
+	this->arr = new T[0];
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	std::cout << "destructor called" << std::endl;
+	delete[] this->arr;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n) : arr_len(n), arr(nullptr)
+{
+	this->arr = new T[n];
+}
+
+template <typename T>
+Array<T>::Array(Array const &copy)
+{
+	*this = copy;
+}
+
+template <typename T>
+Array<T>& Array<T>::operator=(Array const &array)
+{
+	if (this == &array)
+		return (*this);
+
+	if (this->arr)
+	{
+		delete[] this->arr;
+		this->arr = nullptr;
+	}
+	this->arr_len = array.arr_len;
+	this->arr = new T[this->arr_len];
+	for (unsigned int i = 0; i < this->arr_len; ++i)
+		arr[i] = array.arr[i];
+
+	return (*this);
+}
+
+template <typename T>
+std::exception Array<T>::IndexOutOfRange() const
+{
+	throw std::out_of_range("The index is out of range!");
+}
+
+template <typename T>
+T& Array<T>::operator[](const unsigned int index) const
+{
+	if (index < 0 || index >= this->arr_len)
+		Array<T>::IndexOutOfRange();
+
+	return (arr[index]);
+}
+
+template <typename T>
+unsigned int	Array<T>::size(void) const
+{
+	return (this->arr_len);
+}
 
 #endif
--- CPP_MODULE_07/ex02/main.cpp
+++ CPP_MODULE_07/ex02/main.cpp
@@ -2,6 +2,72 @@
 
 int main(void)
 {
+	Array<int> arr;
 
+	Array<int> arr1(5);
+	arr = arr1;
+
+	std::cout << " ============ " << std::endl;
+	for (unsigned int i = 0; i < arr.size(); ++i)
+	{
+		arr[i] = i;
+		std::cout << arr[i] << std::endl;
+	}
+	std::cout << " ============ " << std::endl;
+
+	Array<int> arr_copy(arr);
+	for (unsigned int i = 0; i < arr_copy.size(); ++i)
+	{
+		arr_copy[i] = i;
+		std::cout << arr_copy[i] << "f" << std::endl;
+	}
+	std::cout << " ============ " << std::endl;
+
+	Array<float> arr2(10);
+	for (unsigned int i = 0; i < arr2.size(); ++i)
+	{
+		arr2[i] = i;
+		std::cout << arr2[i] << "f" << std::endl;
+	}
+	std::cout << " ============ " << std::endl;
+
+	Array<double> arr3(7);
+	for (unsigned int i = 0; i < arr3.size(); ++i)
+	{
+		arr3[i] = i;
+		std::cout << arr3[i] << "f" << std::endl;
+	}
+	std::cout << " ============ " << std::endl;
+
+	Array<std::string> arr4(3);
+	for (unsigned int i = 0; i < arr4.size(); ++i)
+	{
+		arr4[i] = 'a' + i;
+		std::cout << arr4[i] << std::endl;
+	}
+	std::cout << " ============ " << std::endl;
+
+	try
+	{
+		std::cout << arr[-1] << std::endl;
+	}
+	catch (std::exception const &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << " ============ " << std::endl;
+
+	try
+	{
+		std::cout << arr[100] << std::endl;
+	}
+	catch (std::exception const &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << " ============ " << std::endl;
+	while (1) { };
 	return (0);
 }
